403e379c328f16ea5cb4fb13ead0bd4b3c17434e
malcolm staso <mstaso1@gmail.com>
2020-04-22 15:25:52 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,88 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [ 
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ]
+  
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+   index_0 = win_combo[0] 
+   index_1 = win_combo[1]
+   index_2 = win_combo[2]
+   
+   position_1 = board[index_0]
+   position_2 = board[index_1]
+   position_3 = board[index_2]
+   
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combo
+  end
+end
+  return false
+end
+  
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end  
+
+def draw?(board)
+  if !won?(board) && full?(board) 
+  return true
+  else
+  return false
+end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) 
+    return true
+  else  
+    false
+  end
+end  
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+    return "X"
+    else
+    return "O"
+   end
+  end
+end  
+
+
+
+    
+    
+    
+    
+
+  
+  
+  
+  #for each win_combination in WIN_COMBINATIONS
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  #win_index_1 = win_combination[0]
+  ##win_index_3 = win_combination[2]
+ 
+ # position_1 = board[win_index_1] # load the value of the board at win_index_1
+ # position_2 = board[win_index_2] # load the value of the board at win_index_2
+ # position_3 = board[win_index_3] # load the value of the board at win_index_3
+ 
