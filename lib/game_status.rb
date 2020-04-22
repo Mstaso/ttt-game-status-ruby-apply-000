1d842e7b71d86fbd5bc03db3a1900f5c695a4f3c
malcolm staso <mstaso1@gmail.com>
2020-04-22 13:47:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,11 +15,6 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
   ]
   
-#  0 | 1 | 2  
-#-----------      
-#  3 | 4 | 5   
-#-----------   
-#  6 | 7 | 8   
   
 def won?
   WIN_COMBINATIONS.each {|win_combo|
